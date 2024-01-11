6e5903a0797b527dc4aaf5e8c4e8dcc04e332872
ruijosecj <ruijosecj@gmail.com>
2024-01-11 11:50:20 -0300

    finaliazado o projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+AOS.init();
+
+const dataAniversario = new Date("Jan 29, 2024 19:00:00");
+const timeStampAniversario = dataAniversario.getTime();
+
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampAniversario - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs)/ minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs)/ 1000);
+    
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+    if (distanciaAteEvento < 0 ){
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000);
